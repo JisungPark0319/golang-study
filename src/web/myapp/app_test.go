4ab54b3b4e678c6745c145d8b67ddf49fcb51f03
jisung <wltjd9870@naver.com>
2021-05-05 23:22:06 +0900

    web_test

---
+++ src/web/myapp/app_test.go
@@ -0,0 +1,58 @@
+// go get github.com/smartystreets/goconvey
+// go get github.com/stretchr/testify/assert
+
+package myapp
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexPathHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TestIndexPathHandler_WithoutName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello World!", string(data))
+}
+
+func TestIndexPathHandler_WithName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?name=tucker", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello tucker!", string(data))
+}
